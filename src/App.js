58373caf5ad447f4cfddc2588daf6963bfa8c6e3
Tanish8923 <tg6398119861@gmail.com>
2024-10-27 20:08:28 +0530

    modified the project to meet the requirements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState} from 'react';
+import Tours from './components/Tours';
+import data from './Data';
 
 function App() {
+   
+  const [tours , setTours] = useState(data);
+
+  function removeTourHandler(id){
+        const newTours = tours.filter((tour) => tour.id !== id);
+        setTours(newTours);
+  }
+
+  if(tours.length === 0){
+    return (
+      <div className='refresh'>
+        <h4>No Tour Left</h4>
+        <button className='refreshBtn' onClick={() => setTours(data)}>Refresh</button>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2 className='title'>All Available Tours</h2>
+        <Tours tours={tours} removeTourHandler={removeTourHandler}/>
     </div>
   );
 }
